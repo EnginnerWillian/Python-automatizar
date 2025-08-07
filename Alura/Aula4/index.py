dcf2bd55f4b8a5b1e410fd4422bd5f99e54c3043
WillianPython <williansuportetech@gmail.com>
2025-08-07 20:47:07 -0300

    add tipos de gráficos com bibliotecas diferentes

---
+++ Alura/Aula4/index.py
@@ -0,0 +1,27 @@
+from pandas import pd
+from metplotlib import plt
+import seabornß
+
+
+
+
+
+
+
+
+#Gráficos de torta
+remote_contagens = df_limpo['remoto'_value_counts().reset_index()]
+remote_contagens.columns ['tipo_trabalho', 'quatidades']
+
+
+fig = px.pie(remoto_contagem,
+             names='tipo_trabalho',
+             values='quantidade',
+title='Proporção dos tipos de  trabalho'
+)
+
+
+#Gráfico de barras
+
+df_lipo['senioridade'.value_counts().plot(kind='bar',title="Distruibuição de seneriodade")]
+
